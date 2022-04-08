8d57637584ff7bc1ecc925afa2c90a05f87f5ea7
Matt-S-Elliott <mse951993@gmail.com>
2022-04-08 14:06:51 -0600

    Comment transition codes properly

--- enumDefinitions.py
+++ enumDefinitions.py
@@ -12,22 +12,19 @@ class ClassKeyStates(Enum):
 
 
 class JiraTransitionCodes(Enum):
-    """
-    Start Progress: 11 (From Open to In Progress)
-    Ready for review: 21 (From In Progress to UNDER REVIEW)
-    Stop Progress: 111 (From In Progress to CANCELLED)
-    Approve : 31 (From Under Review to APPROVED)
-    Reject: 131 (From Under Review to REJECTED)
-    Done: 41  (From APPROVED to DONE)
-    Reopen: 121  (From Cancelled to OPEN)
-    Restart progress : 141  (From REJECTED to IN PROGRESS) # Renamed from "Start progress" to "Restart progress" when changing these to enums
-    """
-
     START_PROGRESS = 11
+    """Start Progress: 11 (From Open to In Progress)"""
     READY_FOR_REVIEW = 21
+    """Ready for review: 21 (From In Progress to UNDER REVIEW)"""
     STOP_PROGRESS = 111
+    """Stop Progress: 111 (From In Progress to CANCELLED)"""
     APPROVE = 31
+    """Approve : 31 (From Under Review to APPROVED)"""
     REJECT = 131
+    """Reject: 131 (From Under Review to REJECTED)"""
     DONE = 41
+    """Done: 41  (From APPROVED to DONE)"""
     REOPEN = 121
+    """Reopen: 121  (From Cancelled to OPEN)"""
     RESTART_PROGRESS = 141
+    """Restart progress : 141  (From REJECTED to IN PROGRESS) # Renamed from "Start progress" to "Restart progress" when changing these to enums"""
