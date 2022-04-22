35d7f15a170811adaf7f7edf09b49a7a2e1f82c1
Matt-S-Elliott <mse951993@gmail.com>
2022-04-22 15:26:37 -0600

    Add pony DB setup to main.

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import schedule
 import time
 import octoprint
 import yaml
+from pony.orm import *
+from classes.printer import *
+from classes.enumDefinitions import *
+
+# set_sql_debug(True)  # Shows the SQL queries pony is running in the console.
+db.bind(provider='sqlite', filename='octofarmJira_database.sqlite', create_db=True)  # Establish DB connection.
+db.provider.converter_classes.append((Enum, EnumConverter))  # Add a custom enum converter to pony so we can use enums in the DB.
+db.generate_mapping(create_tables=True)  # Required by pony.
+
 
 with open("config_files/config.yml", "r") as yamlfile:
     config = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
-### we start the services from the start ###
 jira.getGcode()
 octoprint.eachNewFile()
 octoprint.PrintIsFinished()
 
-### Then the system loops the schedules functions ###
 print("PRINT MONITORING SYSTEM LOOP STARTED")
 schedule.every(config['updateRate']).minutes.do(jira.getGcode)
 schedule.every(config['updateRate']).minutes.do(octoprint.eachNewFile)
