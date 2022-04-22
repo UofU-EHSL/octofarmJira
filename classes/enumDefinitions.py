bdd5cc06fc27bdca19a4aec8cfb38a9cab19d099
Matt-S-Elliott <mse951993@gmail.com>
2022-04-22 16:57:30 -0600

    Remove enum method. Not worth the trouble. Code is in testing state, so it doesn't make sense right now.

--- classes/enumDefinitions.py
+++ classes/enumDefinitions.py
@@ -2,25 +2,6 @@ from enum import Enum, auto
 from pony.orm.dbapiprovider import StrConverter
 
 
-class EnumConverter(StrConverter):
-    """
-    Needed to make enums work with Pony ORM.
-    """
-
-    def validate(self, val, not_used):
-        """not_used param was required to keep it from crashing... Not sure what its needed for. Things seem to be working without it."""
-        if not isinstance(val, Enum):
-            raise ValueError('Must be an Enum.  Got {}'.format(type(val)))
-        return val
-
-    def py2sql(self, val):
-        return val.name
-
-    def sql2py(self, value):
-        # Any enum type can be used, so py_type ensures the correct one is used to create the enum instance
-        return self.py_type[value]
-
-
 class GcodeStates(Enum):
     VALID = 1
     INVALID = 2
--- main.py
+++ main.py
@@ -7,25 +7,42 @@ from pony.orm import *
 from classes.printer import *
 from classes.enumDefinitions import *
 
-# set_sql_debug(True)  # Shows the SQL queries pony is running in the console.
-db.bind(provider='sqlite', filename='octofarmJira_database.sqlite', create_db=True)  # Establish DB connection.
-db.provider.converter_classes.append((Enum, EnumConverter))  # Add a custom enum converter to pony so we can use enums in the DB.
-db.generate_mapping(create_tables=True)  # Required by pony.
+
+def main():
+    set_sql_debug(True)  # Shows the SQL queries pony is running in the console.
+    db.bind(provider='sqlite', filename='octofarmJira_database.sqlite', create_db=True)  # Establish DB connection.
+    db.generate_mapping(create_tables=True)
+
+    # db.generate_mapping(check_tables=False, create_tables=False)
+    # db.drop_all_tables(with_all_data=True)
+    # db.create_tables()
+    #
+    # with db_session:
+    #     p1 = Printer(name='Prusa01', model=PrinterModel.PRUSA_MK3.name, ip='localhost:81', api_key='53148701F56E47368C7737DF546B1532')
+    #     p2 = Printer(name='Prusa02', model=PrinterModel.PRUSA_MK3.name, ip='localhost:82', api_key='2ECDDF5FCFF44C56A4E864AFC1ABABD9')
+    #     p3 = Printer(name='Prusa03', model=PrinterModel.PRUSA_MK3.name, ip='localhost:83', api_key='A004159B89CB4226BED7E66A442A76F6')
+    #     p4 = Printer(name='Prusa04', model=PrinterModel.PRUSA_MK3.name, ip='localhost:84', api_key='0E00C61D6C964722A0D39B3D2CD98DBA')
+    #     p5 = Printer(name='Prusa05', model=PrinterModel.PRUSA_MK3.name, ip='localhost:85', api_key='44D69C98F8B54EEA827988AFE667BA0A')
+
+    with open("config_files/config.yml", "r") as yamlfile:
+        config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+    printers = Printer.Get_All()
+
+    # jira.getGcode()
+    # octoprint.eachNewFile()
+    # octoprint.PrintIsFinished()
+    #
+    # print("PRINT MONITORING SYSTEM LOOP STARTED")
+    # schedule.every(config['updateRate']).minutes.do(jira.getGcode)
+    # schedule.every(config['updateRate']).minutes.do(octoprint.eachNewFile)
+    # schedule.every(config['updateRate']).minutes.do(octoprint.PrintIsFinished)
+    # schedule.every(config['updateRate']).minutes.do(jira.askedForStatus)
+
+    # while 1:
+    #     schedule.run_pending()
+    #     time.sleep(config['updateRate'])
 
 
-with open("config_files/config.yml", "r") as yamlfile:
-    config = yaml.load(yamlfile, Loader=yaml.FullLoader)
-
-jira.getGcode()
-octoprint.eachNewFile()
-octoprint.PrintIsFinished()
-
-print("PRINT MONITORING SYSTEM LOOP STARTED")
-schedule.every(config['updateRate']).minutes.do(jira.getGcode)
-schedule.every(config['updateRate']).minutes.do(octoprint.eachNewFile)
-schedule.every(config['updateRate']).minutes.do(octoprint.PrintIsFinished)
-schedule.every(config['updateRate']).minutes.do(jira.askedForStatus)
-
-while 1:
-    schedule.run_pending()
-    time.sleep(config['updateRate'])
+if __name__ == "__main__":
+    main()
