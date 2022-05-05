64d53d747aa4cc59b3609ee904d2b5d8599ac3b4
Matt-S-Elliott <mse951993@gmail.com>
2022-05-05 14:28:13 -0600

    Add test permission code data.

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ def drop_and_create_db():
         p4 = Printer(name='Prusa04', model=PrinterModel.PRUSA_MK3.name, ip='localhost:84', api_key='0E00C61D6C964722A0D39B3D2CD98DBA', enabled=True, material_density=1.25)
         p5 = Printer(name='Prusa05', model=PrinterModel.PRUSA_MK3.name, ip='localhost:85', api_key='44D69C98F8B54EEA827988AFE667BA0A', enabled=True, material_density=1.25)
 
+        pc1 = PermissionCode(name='Test1', code='abcd', description='No dates')
+        pc2 = PermissionCode(name='Test2', code='abcde', description='Start Today', start_date=datetime.date.today())
+        pc3 = PermissionCode(name='Test3', code='abcdef', description='Start Tomorrow', start_date=datetime.date.today() + datetime.timedelta(days=1))
+        pc4 = PermissionCode(name='Test4', code='abcdefg', description='End Today', end_date=datetime.date.today())
+        pc5 = PermissionCode(name='Test5', code='abcdefgh', description='End Tomorrow', end_date=datetime.date.today() + datetime.timedelta(days=1))
+        pc6 = PermissionCode(name='Test6', code='abcdefghi', description='Both dates valid', start_date=datetime.date.today() - datetime.timedelta(days=1), end_date=datetime.date.today() + datetime.timedelta(days=1))
+        pc6 = PermissionCode(name='Test7', code='abcdefghij', description='Both dates not valid', start_date=datetime.date.today() - datetime.timedelta(days=2), end_date=datetime.date.today() - datetime.timedelta(days=1))
 
 def main():
     set_sql_debug(True)  # Shows the SQL queries pony is running in the console.
